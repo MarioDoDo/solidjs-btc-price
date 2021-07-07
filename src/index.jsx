cf87bd7b91e72508371a3f977fd8faa4784041f7
MarioDoDo <75090058+MarioDoDo@users.noreply.github.com>
2021-07-08 00:05:38 +0200

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import { createSignal, onCleanup } from "solid-js";
+import { render } from "solid-js/web";
+
+const Price = () => {
+let z;
+
+function Get() {
+    fetch(`https://api.cryptonator.com/api/ticker/btc-usd`)
+        .then(res => res.json())
+        .then(data => z = Number(data.ticker.price).toFixed(2));
+        }
+        Get();
+
+  const [price, setPrice] = createSignal(0),
+    fn = setInterval(() => {
+    Get();
+    setPrice(z)}, 1000);
+    setPrice();
+  return <div><p>{price()}$</p></div>;
+};
+
+render(() => <Price />, document.getElementById("app"));
